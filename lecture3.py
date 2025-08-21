8a49fdb21f863f863e240f5d5e93d8590e6a45d4
Ameer <amir.m.abukhalaf@gmail.com>
2025-08-21 21:47:13 +0300

    List Exercise

---
+++ lecture3.py
@@ -0,0 +1,26 @@
+fruits = [ "Peach"," Grapes" , "Pineapple","Watermelon","Apple" ]
+print(fruits)
+
+# 2
+print(fruits[0],fruits[-1])
+
+# 3
+fruits[1] = "Mango"
+print(fruits)
+
+#4
+fruits.insert(1,"Watermelon" )
+print(fruits)
+#5
+name = input("Enter The fruit name ")
+print(fruits.count(name) != 0)
+
+#6
+fruits.sort()
+print(fruits[0:])
+
+
+
+
+
+          
